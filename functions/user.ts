6271ff55e10b7d058b4fc2cf28b65842fd6d0a78
wadaryohei <ryohei.w0317wr@icloud.com>
2020-09-24 11:32:30 +0900

    #2 add ユーザー登録時にDynamoDBにユーザー情報を格納するfunctionsを追加

---
+++ functions/user.ts
@@ -0,0 +1,30 @@
+import AWS = require('aws-sdk');
+import { CognitoUserPoolTriggerEvent, Context, Callback } from 'aws-lambda';
+
+// ------------------------------------------
+// User作成完了時ユーザー情報をUserテーブルに書き込む
+// ------------------------------------------
+export const handler = (event: CognitoUserPoolTriggerEvent, callback: Callback) => {
+
+  // DynamoDBを初期化
+  const dynamoDB = new AWS.DynamoDB.DocumentClient({
+    region: "ap-northeast-1"
+  });
+
+  // DBに保存する値
+  const params = {
+    TableName: 'my-serverless-backend-dev-User',
+    Item: {
+      'userId': event.userName,
+      "username": event.userName,
+      "email": event.request.userAttributes.email
+    }
+  }
+
+  // DBに保存
+  dynamoDB.put(params).promise().then(() => {
+    callback(null, event)
+  }).catch((error) => {
+    callback(new Error(`${error} Couldn\'t create the todo item.`), event)
+  })
+}
